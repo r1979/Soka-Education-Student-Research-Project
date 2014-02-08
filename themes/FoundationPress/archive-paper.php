ba6f0d65448a6318150059dba513f4cc0e00d684
RememberLenny <leonard_bogdonoff@condenast.com>
2014-02-08 02:25:57 -0500

    Save the query

--- themes/FoundationPress/archive-paper.php
+++ themes/FoundationPress/archive-paper.php
@@ -3,11 +3,11 @@
 <!-- Row for main content area -->
 	<div class="row head-top">
 		<div class="column">
-<div class="column">		
+<div class="column">
 <h1>Vote for published articles.</h1>
 <h2>Search by authors, titles, or conference year.</h2>
 <?php get_search_form(); ?>
-</div>		
+</div>
 </div>
 	</div>
 
@@ -15,8 +15,8 @@
 	<?php if ( have_posts() ) : ?>
 		<ul class="large-block-grid-3 medium-block-grid-2 small-block-grid-1">
 		<?php /* Start the Loop */ ?>
+		<?php $query = new WP_Query( array ( 'orderby' => 'rand' )); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
-
 			<?php get_template_part( 'content', 'paper' ); ?>
 		<?php endwhile; ?>
 
