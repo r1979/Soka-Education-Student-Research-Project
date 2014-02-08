60fa2d87b304485be8a1a486297ba037eeeff1c5
RememberLenny <leonard_bogdonoff@condenast.com>
2014-02-07 23:02:40 -0500

    Save the import

--- themes/FoundationPress/paper-full.php
+++ themes/FoundationPress/paper-full.php
@@ -2,26 +2,33 @@
 /*
 Template Name: Paper Full
 */
+
 get_header(); ?>
 
-	<?php
-		$args = array(
-			'post_type'=> 'paper',
-			'order'   => 'ASC'
-		);
+<!-- Row for main content area -->
+	<div class="small-12 large-8 columns" role="main">
 
-		$the_query = new WP_Query( $args );
-	?>
+	<?php if ( have_posts() ) : ?>
+		<ul class="large-block-grid-6 medium-block-grid-4 small-block-grid-2">
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php endwhile; ?>
 
-	<div class="small-12 large-12 columns" role="main">
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		</ul>
+	<?php endif; // end have_posts() check ?>
 
-	<?php /* Start loop */ ?>
-	<ul class="large-block-grid-6 medium-block-grid-4 small-block-grid-2">
-		<?php while (have_posts()) : the_post(); ?>
-		<?php	get_template_part( 'loop', 'paper	' ); ?>
-		<?php endwhile; // End the loop ?>
-	</ul>
+	<?php /* Display navigation to next/previous pages when applicable */ ?>
+	<?php if ( function_exists('reverie_pagination') ) { reverie_pagination(); } else if ( is_paged() ) { ?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
+		</nav>
+	<?php } ?>
 
 	</div>
+	<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
