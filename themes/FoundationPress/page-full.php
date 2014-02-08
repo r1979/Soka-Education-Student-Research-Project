3db4b34f4c17becd036fad064d32b926454e9450
RememberLenny <leonard_bogdonoff@condenast.com>
2014-02-07 23:32:57 -0500

    Save single paper

--- themes/FoundationPress/page-full.php
+++ themes/FoundationPress/page-full.php
@@ -1,20 +1,28 @@
 <?php get_header(); ?>
 
+<!-- Row for main content area -->
 	<div class="small-12 large-12 columns" role="main">
 
-	<?php while (have_posts()) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-			</header>
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div>
-			<footer>
-				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
-			</footer>
-		</article>
-	<?php endwhile;?>
+	<?php if ( have_posts() ) : ?>
+		<ul class="large-block-grid-4 medium-block-grid-3 small-block-grid-2">
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content', 'paper' ); ?>
+		<?php endwhile; ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		</div>
+	<?php endif; // end have_posts() check ?>
+
+	<?php /* Display navigation to next/previous pages when applicable */ ?>
+	<?php if ( function_exists('reverie_pagination') ) { reverie_pagination(); } else if ( is_paged() ) { ?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
+		</nav>
+	<?php } ?>
 
 	</div>
 
--- themes/FoundationPress/single-paper.php
+++ themes/FoundationPress/single-paper.php
@@ -1,16 +1,25 @@
 <?php get_header(); ?>
 
-	<div class="small-12 large-8 columns" role="main">
+	<div class="small-12 large-12 columns" role="main">
 
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php FoundationPress_entry_meta(); ?>
+
 			</header>
 			<div class="entry-content">
-				<?php the_content(); ?>
+				<?php the_field( "paper_abstract" ); ?>
+				<span class="authors">
+					<?php the_field( "authors" ); ?>
+				</span>
 			</div>
+				<span class="pub_issue">
+					<?php the_field( "published_issue" ); ?>,
+				</span>
+				<span class="pub_pages">
+					<?php the_field( "published_pages" ); ?>
+				</span>
 		</article>
 	<?php endwhile;?>
 
