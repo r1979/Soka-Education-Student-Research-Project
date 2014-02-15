f6dc0055be3734adc0e2cc383454092c144f4801
RememberLenny <leonard_bogdonoff@condenast.com>
2014-02-15 15:58:51 -0800

    Create the table-full

--- themes/FoundationPress/table-full.php
+++ themes/FoundationPress/table-full.php
@@ -5,13 +5,19 @@ Template Name: Table Full
 
 ?>
 <?php get_header(); ?>
-  <div class="row head-top">
+
+  <div class="row">
     <div class="column">
 
       <table id="table">
         <thead>
           <tr>
-            <th>Papers</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Issue</th>
+            <th>Location</th>
+            <th>Read</th>
+            <th>Vote</th>
           </tr>
         </thead>
         <tbody id="tbody"></tbody>
@@ -20,29 +26,6 @@ Template Name: Table Full
     </div>
   </div>
 
-<script id="tbody-content" type="type/x-handlebars-template">
-  <tr>
-    {{#each posts}}
-      <td>{{title}}</td>
-      {{#each taxonomy_authors}}
-      <td>{{title}}</td>
-      {{/each}}
-      {{#each taxonomy_published_issue}}
-      <td>Published: {{title}}</td>
-      {{/each}}
-      <td>{{custom_fields.published_pages}}</td>
-    {{/each}}
-  </tr>
-</script>
-
-<script>
-  var source    = $('#tbody').html();
-  var template  = Handlebars.compile(source);
-  var context   = jQuery.getJSON('curl_file.json', function( data ){});
-  var html      = template(context);
-</script>
-
-
-
+<?php get_template_part('handlebars', 'table'); ?>
 
 <?php get_footer(); ?>
